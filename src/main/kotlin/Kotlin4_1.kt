c41fc48e6e52b52ff2435890161c493c2aa46954
Vlad Fisenko <Vladf.fisenko.007@gmail.com>
2025-04-15 23:56:02 +0600

    add Note and NoteComments

--- src/main/kotlin/Kotlin4_1.kt
+++ src/main/kotlin/Kotlin4_1.kt
@@ -1,6 +1,12 @@
 class PostNotFoundException(message: String) : RuntimeException(message)
 class CommentNotFoundException(message: String) : RuntimeException(message)
 class ReasonNotFoundException(message: String) : RuntimeException(message)
+class NoteNotFoundException(message: String) : RuntimeException(message)
+class CommentNoteNotFoundException(message: String) : RuntimeException(message)
+
+data class Note(val id: Int, val title: String, val text: String, val date: String)
+
+data class NotesComment(val noteId: Int, val cId: Int, val message: String)
 
 data class ReportComments(val ownerId: Int, val commentId: Int, val reason: Int)
 
@@ -52,9 +58,14 @@ data class Post(
 
 object WallService {
     private var numberPost = 0
+    private var numberNote = 0
+    private var numberNoteComment = 0
     private var posts = emptyArray<Post>()
     private var comments = emptyArray<Comment>()
     private var reportComments = emptyArray<ReportComments>()
+    private var notes: MutableList<Note> = mutableListOf()
+    private var notesComment: MutableList<NotesComment> = mutableListOf()
+    private var deleteNotesComment: MutableList<NotesComment> = mutableListOf()
     fun add(post: Post): Post {
         numberPost++
         posts += post.copy(id = numberPost)
@@ -94,12 +105,125 @@ object WallService {
         return false
     }
 
+    fun notesAdd(title: String, text: String, date: String): Int {
+        numberNote++
+        notes.add(Note(numberNote, title, text, date))
+        return numberNote
+    }
+
+    fun notesDeleteComment(commentId: Int): Boolean {
+        for ((index) in notesComment.withIndex()) {
+            if (commentId == notesComment[index].cId) {
+                deleteNotesComment += notesComment[index]
+                notesComment.removeAt(index)
+                return true
+            }
+        }
+        throw CommentNoteNotFoundException("No element with $commentId")
+    }
+
+    fun notesEditComment(commentId: Int, message: String): Boolean {
+        for ((index) in notesComment.withIndex()) {
+            if (commentId == notesComment[index].cId) {
+                val idNote = notesComment[index].noteId
+                notesComment.removeAt(index)
+                notesComment.add(index, NotesComment(idNote, commentId, message))
+                return true
+            }
+        }
+        throw CommentNoteNotFoundException("No element with $commentId")
+    }
+
+    fun notesDelete(id: Int): Boolean {
+        for ((index) in notes.withIndex()) {
+            if (id == notes[index].id) {
+                notes.removeAt(index)
+                notesDeleteComment(id)
+                return true
+            }
+        }
+        throw NoteNotFoundException("No element with $id")
+    }
+
+    fun notesEdit(id: Int, title: String, text: String, date: String): Boolean {
+        for ((index) in notes.withIndex()) {
+            if (id == notes[index].id) {
+                notes.removeAt(index)
+                notes.add(index, Note(id, title, text, date))
+                return true
+            }
+        }
+        throw NoteNotFoundException("No element with $id")
+    }
+
+    fun notesGet(noteIds: MutableList<Int>): MutableList<Note> {
+        val notesGet: MutableList<Note> = mutableListOf()
+        for ((index1) in noteIds.withIndex()) {
+            for ((index2) in notes.withIndex()) {
+                if (noteIds[index1] == notes[index2].id) {
+                    notesGet.add(notes[index2])
+                }
+            }
+        }
+        return notesGet
+    }
+
+    fun notesGetById(noteId: Int): Note {
+        for ((index) in notes.withIndex()) {
+            if (noteId == notes[index].id) {
+                return notes[index]
+            }
+        }
+        throw NoteNotFoundException("No element with $noteId")
+    }
+
+    fun notesRestoreComment(commentId: Int): Boolean {
+        for ((index) in deleteNotesComment.withIndex()) {
+            if (commentId == deleteNotesComment[index].cId) {
+                notesComment.add(deleteNotesComment[index])
+                deleteNotesComment.removeAt(index)
+                return true
+            }
+        }
+        throw CommentNoteNotFoundException("No element with $commentId")
+    }
+
+    fun createCommentNotes(noteId: Int, message: String): Int {
+        for ((index) in notes.withIndex()) {
+            if (noteId == notes[index].id) {
+                numberNoteComment++
+                val createdNoteComment = NotesComment(noteId, numberNoteComment, message)
+                notesComment.add(createdNoteComment)
+                return numberNoteComment
+            }
+        }
+        throw NoteNotFoundException("No element with $noteId")
+    }
+
+    fun notesGetComment(noteId: Int): MutableList<NotesComment> {
+        val notesGetComment: MutableList<NotesComment> = mutableListOf()
+        for ((index) in notesComment.withIndex()) {
+            if (noteId == notesComment[index].noteId) {
+                notesGetComment += notesComment[index]
+            }
+        }
+        return notesGetComment
+    }
+
     fun clear() {
         posts = emptyArray()
         numberPost = 0
+        numberPost = 0
+        numberNote = 0
+        numberNoteComment = 0
+        posts = emptyArray<Post>()
+        comments = emptyArray<Comment>()
+        reportComments = emptyArray<ReportComments>()
+        notes = mutableListOf()
+        notesComment = mutableListOf()
+        deleteNotesComment = mutableListOf()
     }
 }
 
 fun main() {
-
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -138,7 +138,7 @@ class WallServiceTest {
     }
 
     @Test(expected = ReasonNotFoundException::class)
-    fun reportCommentWithMoreReasonException() {
+    fun reportCommentWithReasonException() {
         val service = WallService
         val commentsPost = Comments()
         val reportComment = ReportComments(1, 1, 9)
@@ -160,26 +160,206 @@ class WallServiceTest {
         service.reportComment(reportComment)
     }
 
-    @Test(expected = ReasonNotFoundException::class)
-    fun reportCommentWithLessReasonException() {
+    @Test
+    fun notesAdd() {
         val service = WallService
-        val commentsPost = Comments()
-        val reportComment = ReportComments(1, 1, -1)
-        val comment = Comment(
-            1,
-            5,
-            12042025,
-            "text",
-            null,
-            1,
-            1,
-            null,
-            null,
-            null
-        )
-        val post = Post(122, commentsPost, null, 643, 912, null)
-        service.add(post)
-        service.createComment(1, comment)
-        service.reportComment(reportComment)
+        val result = service.notesAdd("title", "text", "15042025")
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun notesDelete() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        val result = service.notesDelete(2)
+        assertEquals(true, result)
+    }
+
+    @Test(expected = NoteNotFoundException::class)
+    fun notesDeleteWithException() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        service.notesDelete(0)
+    }
+
+    @Test
+    fun notesEdit() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        val result = service.notesEdit(2, "title3", "text3", "17042025")
+        assertEquals(true, result)
+    }
+
+    @Test(expected = NoteNotFoundException::class)
+    fun notesEditWithException() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        service.notesEdit(0, "title3", "text3", "17042025")
+    }
+
+    @Test
+    fun notesGet() {
+        val note1 = Note(1, "title", "text", "15042025")
+        val note2 = Note(2, "title2", "text2", "16042025")
+        val getExpectedNotes = mutableListOf(note1, note2)
+        val getNotes = mutableListOf(1, 2)
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        service.notesAdd("title3", "text3", "17042025")
+        val result = service.notesGet(getNotes)
+        assertEquals(getExpectedNotes, result)
+    }
+
+    @Test
+    fun notesGetVoid() {
+        val getExpectedNotes = mutableListOf<Note>()
+        val getNotes = mutableListOf(4, 5)
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        service.notesAdd("title3", "text3", "17042025")
+        val result = service.notesGet(getNotes)
+        assertEquals(getExpectedNotes, result)
+    }
+
+    @Test
+    fun notesGetById() {
+        val getById = Note(2, "title2", "text2", "16042025")
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        val result = service.notesGetById(2)
+        assertEquals(getById, result)
+    }
+
+    @Test(expected = NoteNotFoundException::class)
+    fun notesGetByIdWithException() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        service.notesGetById(5)
+    }
+
+    @Test
+    fun createCommentNotes() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        val result = service.createCommentNotes(1, "Message")
+        assertEquals(1, result)
+    }
+
+    @Test(expected = NoteNotFoundException::class)
+    fun createCommentNotesWithException() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.createCommentNotes(2, "Message")
+    }
+
+    @Test
+    fun notesDeleteComment() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.createCommentNotes(1, "Message")
+        val result = service.notesDeleteComment(1)
+        assertEquals(true, result)
+    }
+
+    @Test(expected = NoteNotFoundException::class)
+    fun notesDeleteCommentWithNoteException() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.createCommentNotes(2, "Message")
+        service.notesDeleteComment(1)
+    }
+
+    @Test(expected = CommentNoteNotFoundException::class)
+    fun notesDeleteCommentWithCommentException() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.createCommentNotes(1, "Message")
+        service.notesDeleteComment(2)
+    }
+
+    @Test
+    fun notesEditComment() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.createCommentNotes(1, "Message")
+        val result = service.notesEditComment(1, "Message2")
+        assertEquals(true, result)
+    }
+
+    @Test(expected = CommentNoteNotFoundException::class)
+    fun notesEditCommentWithCommentException() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.createCommentNotes(1, "Message")
+        service.notesEditComment(2, "Message2")
+    }
+
+    @Test(expected = NoteNotFoundException::class)
+    fun notesEditCommentWithNoteException() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.createCommentNotes(2, "Message")
+        service.notesEditComment(1, "Message2")
+    }
+
+    @Test
+    fun notesGetComment() {
+        val service = WallService
+        val comment1 = NotesComment(1, 1, "Message1")
+        val comment2 = NotesComment(1, 2, "Message2")
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        val getExpectedNotesComment = mutableListOf(comment1, comment2)
+        service.createCommentNotes(1, "Message1")
+        service.createCommentNotes(1, "Message2")
+        service.createCommentNotes(2, "Message3")
+        val result = service.notesGetComment(1)
+        assertEquals(getExpectedNotesComment, result)
+    }
+
+    @Test
+    fun notesGetCommentVoid() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        val getExpectedNotesComment = mutableListOf<NotesComment>()
+        service.createCommentNotes(2, "Message1")
+        service.createCommentNotes(2, "Message2")
+        service.createCommentNotes(2, "Message3")
+        val result = service.notesGetComment(1)
+        assertEquals(getExpectedNotesComment, result)
+    }
+
+    @Test
+    fun notesRestoreComment() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        service.createCommentNotes(1, "Message1")
+        service.createCommentNotes(1, "Message2")
+        service.createCommentNotes(2, "Message2")
+        service.notesDeleteComment(2)
+        val result = service.notesRestoreComment(2)
+        assertEquals(true, result)
+    }
+
+    @Test(expected = CommentNoteNotFoundException::class)
+    fun notesRestoreCommentWithException() {
+        val service = WallService
+        service.notesAdd("title", "text", "15042025")
+        service.notesAdd("title2", "text2", "16042025")
+        service.createCommentNotes(1, "Message1")
+        service.createCommentNotes(1, "Message2")
+        service.createCommentNotes(2, "Message2")
+        service.notesDeleteComment(2)
+        service.notesRestoreComment(4)
     }
 }
